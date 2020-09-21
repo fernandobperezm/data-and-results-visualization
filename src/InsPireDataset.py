384dca9d66886156da599c7aa8063ec7e4dfdace
Fernando Benjamín Pérez Maurera <fperezmaurera@gmail.com>
2020-09-21 19:10:19 +0200

    Unified notebooks for data processing into one named Data Processing

--- src/InsPireDataset.py
+++ src/InsPireDataset.py
@@ -28,16 +28,16 @@ class InsPireDataset(object):
 
         self.processed_london_uk_dataset_path = os.path.join(self._dataset_local_extract_path,
                                                              "processed",
-                                                             "london_uk_data.csv")
+                                                             "london_uk_data")
         self.processed_madrid_spa_dataset_path = os.path.join(self._dataset_local_extract_path,
                                                               "processed",
-                                                              "madrid_spa_data.csv")
+                                                              "madrid_spa_data")
         self.processed_rome_it_dataset_path = os.path.join(self._dataset_local_extract_path,
                                                            "processed",
-                                                           "rome_it_data.csv")
+                                                           "rome_it_data")
         self.processed_stuttgart_ger_dataset_path = os.path.join(self._dataset_local_extract_path,
                                                                  "processed",
-                                                                 "stuttgart_ger_data.csv")
+                                                                 "stuttgart_ger_data")
 
         self._heat_demand: Optional[pd.DataFrame] = None
         self._dhw_profile: Optional[pd.Series] = None
@@ -46,31 +46,6 @@ class InsPireDataset(object):
         self.processed_data: Dict = dict()
 
     def _load_all_data(self):
-        keys_with_paths = [(self.LONDON_UK, self._london_dataset_path),
-                           (self.MADRID_SPA, self._madrid_dataset_path),
-                           (self.ROME_IT, self._rome_dataset_path),
-                           (self.STUTTGART_GER, self._stuttgart_dataset_path)]
-
-        for k, file_path in keys_with_paths:
-            data = pd.read_excel(file_path,
-                                 header=0,
-                                 names=["hourofyear", "air_temp"],
-                                 usecols=["hourofyear", "air_temp"],
-                                 dtype={"hourofyear": np.int32,
-                                        "air_temp": np.float32})
-
-            data["timestamp"] = pd.date_range("2017-01-01", freq="H", periods=data.shape[0])
-            data = data.set_index("timestamp")
-
-            self.data[k] = data
-
-    def load_data(self, reload: bool = False) -> Dict:
-        if reload or len(self.data) == 0:
-            self._load_all_data()
-
-        return self.data.copy()
-
-    def _load_all_processed_data(self):
         def calculate_season(data: pd.DataFrame):
             winter_ends = "2017-03-20"
             spring_ends = "2017-06-20"
@@ -90,28 +65,28 @@ class InsPireDataset(object):
             data.loc[fall_mask, "season"] = "fall"
 
             return data["season"]
-
-        keys_with_paths = [(self.LONDON_UK, self.processed_london_uk_dataset_path),
-                           (self.MADRID_SPA, self.processed_madrid_spa_dataset_path),
-                           (self.ROME_IT, self.processed_rome_it_dataset_path),
-                           (self.STUTTGART_GER, self.processed_stuttgart_ger_dataset_path)]
-
+        
+        keys_with_paths = [(self.LONDON_UK, self._london_dataset_path),
+                           (self.MADRID_SPA, self._madrid_dataset_path),
+                           (self.ROME_IT, self._rome_dataset_path),
+                           (self.STUTTGART_GER, self._stuttgart_dataset_path)]
+        
         self._heat_demand = pd.read_excel(self._heat_demand_path)
         self._dhw_profile = pd.read_excel(self._dhw_profile_path, index_col="Hour")
 
         for k, file_path in keys_with_paths:
-            self.processed_data[k] = pd.read_csv(file_path,
-                                                 index_col=0,
-                                                 header=0,
-                                                 parse_dates=True,
-                                                 infer_datetime_format=True,
-                                                 dtype={"air_temp": np.float32,
-                                                        "dayofyear": np.int32,
-                                                        "hourofyear": np.int32,
-                                                        "air_temp_fit": np.float32}
-                                                 )
-            num_rows_in_processed_data = self.processed_data[k].shape[0]
-            num_days = self.processed_data[k].dayofyear.max()
+            data = pd.read_excel(file_path,
+                                 header=0,
+                                 names=["hourofyear", "air_temp"],
+                                 usecols=["hourofyear", "air_temp"],
+                                 dtype={"hourofyear": np.int32,
+                                        "air_temp": np.float32})
+
+            data["timestamp"] = pd.date_range("2017-01-01", freq="H", periods=data.shape[0])
+            data = data.set_index("timestamp")
+            
+            num_rows_in_processed_data = data.shape[0]
+            num_days = data.index.dayofyear.max()
 
             # We select the current city (determined by k and matched with the city_key column)
             # Then we repeat the rows in the dataset for num_rows_in_processed_data times
@@ -121,7 +96,12 @@ class InsPireDataset(object):
             heat_demand_for_city = pd.concat([heat_demand_for_city] * num_rows_in_processed_data)
             heat_demand_for_city = pd.DataFrame(heat_demand_for_city.values,
                                                 columns=heat_demand_for_city.columns,
-                                                index=self.processed_data[k].index)
+                                                index=data.index).astype({"DHW_cons": np.float32,
+                                                                          "SH_cons": np.float32,
+                                                                          "SFH_bldg_tot": np.float32,
+                                                                          "MFH_bldg_tot": np.float32,
+                                                                          "%SH_y": np.float32,
+                                                                          "%DHW_y": np.float32})
 
             # The DHW profile is constant through the year but changes during the day (however, it keeps the same
             # values for the same hour in different days). The InsPire dataset contains an extra hour (2018-01-01
@@ -130,17 +110,38 @@ class InsPireDataset(object):
             dhw_profile_for_city = pd.concat([dhw_profile_for_city, dhw_profile_for_city.head(1)])
             dhw_profile_for_city = pd.DataFrame(dhw_profile_for_city.values,
                                                 columns=dhw_profile_for_city.columns,
-                                                index=self.processed_data[k].index)
+                                                index=data.index).astype({"DHW Profile": np.float32})
 
-            self.processed_data[k][heat_demand_for_city.columns] = heat_demand_for_city
-            self.processed_data[k]["DHW_hourly_consumption_ratio"] = dhw_profile_for_city["DHW Profile"]
-            self.processed_data[k]["season"] = calculate_season(self.processed_data[k])
-            self.processed_data[k]["hour"] = self.processed_data[k].index.hour
-            self.processed_data[k]["dayofweek"] = self.processed_data[k].index.dayofweek
-            self.processed_data[k]["date"] = self.processed_data[k].index.date
+            data[heat_demand_for_city.columns] = heat_demand_for_city
+            data["DHW_hourly_consumption_ratio"] = dhw_profile_for_city["DHW Profile"]
+            data["season"] = calculate_season(data)
+            data["date"] = data.index.date
+            data["month"] = data.index.month
+            data["dayofyear"] = data.index.dayofyear
+            data["dayofweek"] = data.index.dayofweek
+            data["hourofyear"] = (data.index.dayofyear - 1) * 24 + (data.index.hour + 1)
+            data["hour"] = data.index.hour
+
+            self.data[k] = data
+
+    def load_data(self, reload: bool = False) -> Dict:
+        if reload or len(self.data) == 0 or self._heat_demand is None or self._dhw_profile is None:
+            self._load_all_data()
+
+        return self.data.copy()
+
+    def _load_all_processed_data(self):
+        keys_with_paths = [(self.LONDON_UK, self.processed_london_uk_dataset_path),
+                           (self.MADRID_SPA, self.processed_madrid_spa_dataset_path),
+                           (self.ROME_IT, self.processed_rome_it_dataset_path),
+                           (self.STUTTGART_GER, self.processed_stuttgart_ger_dataset_path)]
+
+        for k, file_path in keys_with_paths:
+            self.processed_data[k] = pd.read_parquet(path=f"{file_path}.parquet",
+                                                     engine="pyarrow")
 
     def load_processed_data(self, reload: bool = False):
-        if reload or len(self.processed_data) == 0 or self._heat_demand is None or self._dhw_profile is None:
+        if reload or len(self.processed_data) == 0:
             self._load_all_processed_data()
 
         return self.processed_data.copy()
