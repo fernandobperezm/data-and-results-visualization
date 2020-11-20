737396f34e0bd721eae4514047761aef29d0bd39
selvacalixto <scalixto@eurac.edu>
2020-11-20 16:10:03 +0100

    Add energy prices (residential and industrial)
    Italy, Germany, Spain, and avg. EU

--- src/InsPireDataset.py
+++ src/InsPireDataset.py
@@ -105,7 +105,11 @@ class InsPireDataset(object):
                                                                           "SFH_bldg_tot": np.float32,
                                                                           "MFH_bldg_tot": np.float32,
                                                                           "%SH_y": np.float32,
-                                                                          "%DHW_y": np.float32})
+                                                                          "%DHW_y": np.float32,
+                                                                          "Gas_price_ind": np.float32,
+                                                                          "Gas_price_res": np.float32,
+                                                                          "El_price_ind": np.float32,
+                                                                          "El_price_res": np.float32})
 
             # The DHW profile is constant through the year but changes during the day (however, it keeps the same
             # values for the same hour in different days). The InsPire dataset contains an extra hour (2018-01-01
